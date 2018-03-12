56788684bf6a894cedaad1eb1fa5bcf3d7745517
yomaliSigera <yomalisigera@gmail.com>
2018-03-12 16:52:51 +0530

    basic pages

---
+++ app/src/main/java/com/example1c/user/heroapp/HeroCharacter1.java
@@ -0,0 +1,14 @@
+package com.example1c.user.heroapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HeroCharacter1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hero_character1);
+        setTitle("S.Mahinda Thero");
+    }
+}
--- app/src/main/java/com/example1c/user/heroapp/MainActivity.java
+++ app/src/main/java/com/example1c/user/heroapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example1c.user.heroapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void click(View v) {
+        Intent intent = new Intent();
+        switch(v.getId()) {
+            case R.id.txtHero1:
+                intent = new Intent(this, HeroCharacter1.class);
+                break;
+           // case R.id.albertB: // R.id.textView2
+             //   intent = new Intent(this, ThirdClass.class);
+            //    break;
+        }
+        startActivity(intent);
+
+
     }
 }
